70b1ec23e77ffe3841d7229d7cb89a51333145d6
lnyuqian <lnyuqian@users.noreply.github.com>
2026-08-09 00:21:29 +0800

    Scrollbar bottom reserve: 48px → 16px

--- src/components/notes/NoteEditor.tsx
+++ src/components/notes/NoteEditor.tsx
@@ -407,7 +407,7 @@ const NoteEditor: React.FC = React.memo(() => {
             {isEditing ? (
                 <div
                     className="flex-1 min-h-0 flex flex-col no-drag-region"
-                    style={{ marginBottom: 48 }}
+                    style={{ marginBottom: 16 }}
                     onDoubleClick={exitEditMode}
                 >
                     <MarkdownEditor
@@ -424,13 +424,13 @@ const NoteEditor: React.FC = React.memo(() => {
             ) : looksLikeMarkdown(activeNote.content) ? (
                 <div
                     className="md-render flex-1 min-h-0 overflow-y-auto overflow-x-hidden no-drag-region custom-scrollbar"
-                    style={{ fontSize: `${editorFontSize}px`, lineHeight: String(editorLineHeight), paddingLeft: `${panelPaddingLeft}%`, paddingRight: `${panelPaddingRight}%`, marginBottom: 48 }}
+                    style={{ fontSize: `${editorFontSize}px`, lineHeight: String(editorLineHeight), paddingLeft: `${panelPaddingLeft}%`, paddingRight: `${panelPaddingRight}%`, marginBottom: 16 }}
                     onDoubleClick={enterEditMode}
                 >
                     <ReactMarkdown remarkPlugins={[remarkGfm]}>{activeNote.content}</ReactMarkdown>
                 </div>
             ) : (
-                <EditorContent editor={editor} className="flex-1 min-h-0 overflow-y-auto overflow-x-hidden no-drag-region" style={{ paddingLeft: `${panelPaddingLeft}%`, paddingRight: `${panelPaddingRight}%`, marginBottom: 48 }} onDoubleClick={enterEditMode} />
+                <EditorContent editor={editor} className="flex-1 min-h-0 overflow-y-auto overflow-x-hidden no-drag-region" style={{ paddingLeft: `${panelPaddingLeft}%`, paddingRight: `${panelPaddingRight}%`, marginBottom: 16 }} onDoubleClick={enterEditMode} />
             )}
         </div>
     );
