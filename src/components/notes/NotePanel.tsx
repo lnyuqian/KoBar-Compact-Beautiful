a6595d5c1a3c018b8615777a2cfe92b710a6965f
lnyuqian <lnyuqian@users.noreply.github.com>
2026-08-08 15:26:00 +0800

    Tab header bg to #424242

--- src/components/notes/NotePanel.tsx
+++ src/components/notes/NotePanel.tsx
@@ -192,7 +192,7 @@ const NotePanel: React.FC = () => {
             {/* Tabs Header */}
             <div
                 className="flex flex-col pt-2 px-2 pb-2 gap-1.5 no-drag-region shrink-0 relative"
-                style={{ backgroundColor: '#323232' }}
+                style={{ backgroundColor: '#424242' }}
             >
                 {/* Tab grid: 2 per row, 93% width, 7% right gap */}
                 <div
